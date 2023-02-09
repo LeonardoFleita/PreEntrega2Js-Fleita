ddb3cc97d5c6354b81867b5db70bf0de03b85834
Leonardo Fleita <leo.fleita@gmail.com>
2023-02-09 02:30:42 -0300

    Ver carta, registrar clientes y agregar productos al carrito

--- .js
+++ .js
@@ -1,18 +1,11 @@
-const clientes = [];
-
-class client{
-    constructor(nombre, apellido, telefono, direccion){
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.telefono = telefono;
-        this.direccion = direccion;
-    };
+function seleccionarProducto(dulceOSalado){
+    let seleccion = prompt(`Escriba el nombre del producto que desee ${productosParaImprimir(dulceOSalado)}`);
+    let productoEncontrado = dulceOSalado.find((el) => el.nombre.toLowerCase() == seleccion.toLowerCase());
+    let cantidad = parseInt(prompt("Escriba el número de la cantidad que desea ordenar"));
+    if(productoEncontrado == undefined){
+    alert("Ha ingresado una opción errónea");
+    }else{
+        const productoAgregado = new productoEnCarrito(productoEncontrado.nombre, productoEncontrado.precio, cantidad);
+        carrito.push(productoAgregado);
+    };     
 };
-
-// let elija = "";
-// while(elija !== "salir"){
-//     productos.forEach((prod)=>{
-//         elija = prompt(`- ${prod.nombre}\n${prod.descripcion}\n$${prod.precio}\n \n Siguiente       Salir`);
-//     });
-// }
-
